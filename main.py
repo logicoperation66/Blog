dd1fd986dade370addf052841ad9601e7f8308b1
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-10-04 22:49:45 +0200

    Zastosowanie funkcji abort

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import redirect
+from flask import abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect('https://www.facebook.com')
+
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return f'<h1>Witaj, {user.name}</h1>'
 
 @app.route('/user/<name>')
 def user(name):
