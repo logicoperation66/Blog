4b1dce967827ff7eace5f5651c427f12ee2a5500
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-10-10 22:38:23 +0200

    Dodanie stron błędów 404 i 500 z bootstrapem

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ def request():
     user_agent = request.headers.get('User-Agent')
     return f'<p>Twoją przeglądarką jest{user_agent}'
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
